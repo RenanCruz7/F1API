803399ad0d6926e744aeb98757c40bfcd4ef3957
RenanCruz7 <renancruz199@gmail.com>
2025-06-30 19:24:06 -0300

    Feat: Criando primeiro controller

---
+++ Controllers/DriverController.cs
@@ -0,0 +1,23 @@
+﻿using F1API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace F1API.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class DriverController : ControllerBase
+{
+    private List<Driver> drivers = new List<Driver>
+    {
+        new Driver {name = "Lewis Hamilton", team = "Mercedes", wins = 100, podiums = 200 },
+        new Driver {name = "Max Verstappen", team = "Red Bull Racing", wins = 64, podiums = 100 },
+        new Driver {name = "Charles Leclerc", team = "Ferrari", wins = 50, podiums = 50}
+    };
+
+    [HttpPost]
+    public void AddDriver([FromBody]Driver driver)
+    {
+        drivers.Add(driver);
+        Console.WriteLine(driver.name);
+        Console.WriteLine(driver.team);
+    }
+}
---
+++ Models/Driver.cs
@@ -0,0 +1,8 @@
+﻿namespace F1API.Models;
+public class Driver
+{
+    public string name { get; set; }
+    public string team { get; set; }
+    public int wins { get; set; }
+    public int podiums { get; set; }
+}
