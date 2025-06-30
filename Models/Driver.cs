aa65c8793791017e5bc5ad51d9603a26d6ccbc9a
RenanCruz7 <renancruz199@gmail.com>
2025-06-30 19:29:14 -0300

    Feat: validação de campos utilizando data annotationss

--- Models/Driver.cs
+++ Models/Driver.cs
@@ -1,8 +1,18 @@
-﻿namespace F1API.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace F1API.Models;
 public class Driver
 {
+    [Required(ErrorMessage="O nome do piloto é obrigatorio")]
+    [StringLength(100, ErrorMessage = "O nome do piloto deve ter no máximo 100 caracteres")]
     public string name { get; set; }
+    [Required(ErrorMessage = "O nome da equipe é obrigatorio")]
+    [StringLength(100, ErrorMessage = "O nome da equipe deve ter no máximo 100 caracteres")]
     public string team { get; set; }
+    [Required(ErrorMessage = "A quantidade de vitórias é obrigatoria")]
+    [Range(0, int.MaxValue, ErrorMessage = "A quantidade de vitórias deve ser um número positivo")]
     public int wins { get; set; }
+    [Required(ErrorMessage = "A quantidade de podius é obrigatoria")]
+    [Range(0, int.MaxValue, ErrorMessage = "A quantidade de podius deve ser um número positivo")]
     public int podiums { get; set; }
 }
